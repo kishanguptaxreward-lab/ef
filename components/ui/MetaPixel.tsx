582ec23f4fbac20e8833fb3328d3b27e8c4f634a
kishanguptaxreward-lab <kishanguptaxreward@gmail.com>
2025-11-19 01:42:59 +0530

    Create MetaPixel.tsx

---
+++ components/ui/MetaPixel.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function MetaPixel() {
+  useEffect(() => {
+    // Facebook Pixel or Meta Pixel can be added here
+    console.log("MetaPixel loaded");
+  }, []);
+
+  return null;
+}
